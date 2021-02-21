b4727e07369d1c606ce1884d0bf20f88df18e405
Connor Rothschild <connorrothschild@gmail.com>
2021-02-21 11:11:17 -0600

    added async loading of images and a placeholder

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,14 +1,28 @@
 <script>
-  import { fade } from "svelte/transition";
 
   export let data;
   export let colors;
+
+  // Promise handling of images while new ones load
+  const loadImage = (url) => new Promise((resolve, reject) => {
+    const img = new Image();
+    img.addEventListener('load', () => resolve(img));
+    img.addEventListener('error', (err) => reject(err));
+    img.src = url;
+  });
+
 </script>
 
 <div class="card">
+  {#await loadImage(data.img_src)}
+    <img src="./assets/placeholder.png" alt="Placeholder image" />
+  {:then}
   <a href={data.youtube_src} target="_blank" rel="noopener noreferrer">
     <img src={data.img_src} alt="A painting titled {data.painting_title}" />
   </a>
+  {:catch error} 
+    <div>{error.message}</div>
+  {/await}
   <h1>{data.painting_title}</h1>
   <h3 style="margin-top: 0; font-weight: 300;">
     Season {data.season}, episode {data.episode}
@@ -57,5 +71,11 @@
     border-radius: 5px;
     box-shadow: 1px 1px 5px 1px #ccc;
     font-size: 0.9rem;
+  }
+
+  img {
+    height: 332px;
+    width: 444px;
+    object-fit: cover;
   }
 </style>
