7c17cad383b280cbc8abf204f726b6cc3f059b94
Connor Rothschild <connorrothschild@gmail.com>
2021-03-10 13:17:17 -0600

    remove deploy attempts

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,7 +1,6 @@
 <script>
   import Meta from "./Meta.svelte";
   import Window from "./helpers/Window.svelte";
-  //   import Header from "./prose/Header.svelte";
   import Main from "./Main.svelte";
   import Footer from "./prose/Footer.svelte";
 </script>
